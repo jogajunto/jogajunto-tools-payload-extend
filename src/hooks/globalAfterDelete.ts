6a3471dbc16d3faa082207de0286b6da40700244
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-17 16:44:39 -0300

    Add hook globalAfterDelete

---
+++ src/hooks/globalAfterDelete.ts
@@ -0,0 +1,31 @@
+import { CollectionAfterDeleteHook } from 'payload/types';
+import { GitData } from '../types';
+import sendAction from '../utilities/actions/github/sendAction';
+import formatSlug from '../utilities/formatSlug';
+
+const globalAfterDelete = (
+  collectionName: string, // Nome da coleção que está sendo modificada
+  directory: string // Diretório que está sendo modificado
+): CollectionAfterDeleteHook => {
+  return async ({ doc }) => {
+    // Prepara os dados para enviar para a Github Action
+    const data: GitData = {
+      event_type: 'delete',
+      client_payload: {
+        slug: doc.slug,
+        operation: 'delete',
+        directory: directory,
+        content: '',
+      },
+    };
+
+    // Envia para o Github Actions
+    sendAction(data);
+
+    // Retorna o documento excluído
+    return doc;
+  };
+};
+
+// Exporta o hook
+export default globalAfterDelete;
