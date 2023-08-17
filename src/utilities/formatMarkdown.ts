02bbaa4c0382ad4ad616c11d7b971d05ed6a94ab
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-17 16:45:56 -0300

    Add refactored formatMarkdown utils

---
+++ src/utilities/formatMarkdown.ts
@@ -0,0 +1,108 @@
+import yaml from 'js-yaml';
+import _ from 'lodash';
+import { Document } from 'payload/types';
+import { CollectionName } from '../types/CollectionName';
+import { Payload } from 'payload';
+
+const formatMarkdown = async (
+  doc: Document,
+  collectionName: CollectionName,
+  payload: Payload,
+  formatters: Object
+) => {
+  const dataFormatter = formatters[collectionName];
+  if (!dataFormatter) {
+    throw new Error(`Formatter for collection "${collectionName}" not found.`);
+  }
+
+  const data = await dataFormatter(doc, payload);
+
+  let content = '\n';
+  // Loop in blocks content
+  for (const block of doc.content) {
+    console.log(block.type);
+    switch (block.type) {
+      case 'h2': // Block type H2
+        content += `## ${block.children[0].text}\n\n`;
+        break;
+
+      case 'upload':
+        // Block type Upload
+        const idUpload = block.value.id;
+        if (idUpload) {
+          const mediaFile = await payload.findByID({
+            collection: 'media',
+            id: idUpload,
+          });
+          if (mediaFile) {
+            const filename = mediaFile.filename;
+            const urlFile = mediaFile.url;
+
+            content += `![${filename}](${urlFile})\n`;
+          }
+        }
+        break;
+
+      case 'ul':
+        // Block type UL
+        block.children.forEach((listItem) => {
+          content += '- ';
+          listItem.children.forEach((child) => {
+            let text = child.text;
+
+            if (child.bold) {
+              text = `**${text}**`;
+            }
+
+            if (child.italic) {
+              text = `_${text}_`;
+            }
+
+            if (child.type === 'link') {
+              child.children.forEach((linkChild) => {
+                text = `[${linkChild.text}](${child.url})`;
+              });
+            }
+
+            content += _.trim(text, '\n');
+          });
+          content += '\n';
+        });
+        break;
+
+      default:
+        if (block.children.text != '') {
+          // Block type any
+          block.children.forEach((child) => {
+            let text = child.text;
+
+            if (child.bold) {
+              text = `**${text}**`;
+            }
+
+            if (child.type === 'link') {
+              text = `[${text}](${child.url})`;
+            }
+
+            if (child.italic) {
+              text = `_${text}_`;
+            }
+
+            content += _.trim(text, '\n');
+          });
+
+          if (block.children.length > 0) {
+            content += '\n\n'; // Add two newlines to create a new paragraph in Markdown
+          }
+        }
+        break;
+    }
+  }
+
+  const yamlText = yaml.dump(data);
+  const fileContent = `---\n${yamlText}---\n${content}`;
+
+  return fileContent;
+};
+
+export default formatMarkdown;
