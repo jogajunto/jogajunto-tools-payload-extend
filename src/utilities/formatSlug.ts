f215a0d1bbc405c27a9c969051b14ec1c1a8ecbc
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-17 16:46:04 -0300

    Add formatSlug utils

---
+++ src/utilities/formatSlug.ts
@@ -0,0 +1,37 @@
+import { FieldHook } from 'payload/types';
+
+const removeAccents = (str) => {
+  const accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ';
+  const accentsOut = 'AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn';
+  return str
+    .split('')
+    .map((letter, index) => {
+      const accentIndex = accents.indexOf(letter);
+      return accentIndex !== -1 ? accentsOut[accentIndex] : letter;
+    })
+    .join('');
+};
+
+const format = (val: string): string =>
+  removeAccents(val)
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '')
+    .toLowerCase();
+
+const formatSlug =
+  (fallback: string): FieldHook =>
+  ({ value, originalDoc, data }) => {
+    if (typeof value === 'string') {
+      return format(value);
+    }
+    const fallbackData =
+      (data && data[fallback]) || (originalDoc && originalDoc[fallback]);
+
+    if (fallbackData && typeof fallbackData === 'string') {
+      return format(fallbackData);
+    }
+
+    return value;
+  };
+
+export default formatSlug;
