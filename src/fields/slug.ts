31f731e47a882ed7e64e0c1fb90d21d35232490b
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-18 00:51:15 -0300

    Add documentation comments for slug field

--- src/fields/slug.ts
+++ src/fields/slug.ts
@@ -1,6 +1,9 @@
 import { Field } from 'payload/types';
 import formatSlug from '../utilities/formatSlug';
 
+/**
+ * Representa o campo "slug".
+ */
 const slug: Field = {
   name: 'slug',
   label: 'Slug',
@@ -15,6 +18,11 @@ const slug: Field = {
   unique: true,
 };
 
+/**
+ * Cria uma variação do campo "slug", com um gatilho customizado para formatação.
+ * @param fieldParam O campo utilizado como base para formatação do slug.
+ * @returns Uma definição de campo "slug" customizada.
+ */
 export const createSlugField = (fieldParam: string): Field => ({
   ...slug,
   hooks: {
