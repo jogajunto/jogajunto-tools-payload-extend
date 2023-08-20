16f1ebaa299e70091f75f87c3b5d127be3b6286b
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-20 20:26:43 -0300

    Add initial code for mediaAferChange.ts

--- src/hooks/mediaAfterChange.ts
+++ src/hooks/mediaAfterChange.ts
@@ -1,1 +1,24 @@
-// TODO
+/**
+ * Hook para lidar com doc que são apenas mídia com um nome/slug
+ */
+import { CollectionAfterChangeHook } from 'payload/types';
+import { CollectionName } from '../types/CollectionName';
+
+// Hook que sera executado após a alteração de um doc que representa somente uma media, para envio ao repositório do Github
+const mediaAfterChange = (
+  collectionName: CollectionName, // Nome da coleção que está sendo modificada
+  directory: string // Diretório que está sendo modificado
+): CollectionAfterChangeHook => {
+  return async ({
+    doc, // Dados completos do documento
+    req: { payload }, // Requisição completa do express, transformada em payload para operações de busca
+    previousDoc, // Dados do documento antes de ser modificado
+    operation, // Nome da operação, ex: 'create', 'update'
+  }) => {
+    console.log(doc);
+    return doc;
+  };
+};
+
+// Exporta o hook
+export default mediaAfterChange;
