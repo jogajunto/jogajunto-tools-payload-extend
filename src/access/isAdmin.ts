5b6fbb9e3d8aae9df52821a34fa6d95ec0b05946
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-20 02:26:01 -0300

    Fix comments in code for access

--- src/access/isAdmin.ts
+++ src/access/isAdmin.ts
@@ -1,16 +1,29 @@
 import { Access, FieldAccess } from 'payload/types';
 import { UserType } from '../types/UserType';
 
+/**
+ * Verifica se um usuário tem a função de 'admin'.
+ *
+ * @param {Object} req - O objeto de solicitação com informações do usuário.
+ * @returns {boolean} Retorna verdadeiro se o usuário tem o papel de 'admin', caso contrário, retorna falso.
+ */
 export const isAdmin: Access<any, UserType> = ({ req: { user } }) => {
-  // Return true or false based on if the user has an admin role
   return Boolean(user?.roles?.includes('admin'));
 };
 
+/**
+ * Verifica a nível de campo se o usuário é um 'admin'.
+ *
+ * Esta função é similar à função 'isAdmin', mas foi projetada para
+ * ser utilizada em verificações de nível de campo.
+ *
+ * @param {Object} req - O objeto de solicitação com informações do usuário.
+ * @returns {boolean} Retorna verdadeiro se o usuário tem o papel de 'admin', caso contrário, retorna falso.
+ */
 export const isAdminFieldLevel: FieldAccess<
   { id: string },
   unknown,
   UserType
 > = ({ req: { user } }) => {
-  // Return true or false based on if the user has an admin role
   return Boolean(user?.roles?.includes('admin'));
 };
--- src/access/isEditor.ts
+++ src/access/isEditor.ts
@@ -1,13 +1,21 @@
 import { Access } from 'payload/types';
 import { UserType } from '../types/UserType';
 
+/**
+ * Verifica se um usuário tem a função de 'editor' ou é o proprietário do documento.
+ *
+ * Se o usuário for um 'admin', concede acesso total.
+ * Se o usuário for um 'editor', restringe o acesso apenas ao próprio usuário.
+ * Caso contrário, nega o acesso.
+ *
+ * @param {Object} req - O objeto de solicitação com informações do usuário.
+ * @returns {boolean|Object} Retorna verdadeiro se tiver permissão total, um objeto de condição se for restrito, ou falso para negar acesso.
+ */
 export const isEditorOrOwnsDocument: Access<any, UserType> = ({ req }) => {
   const user = req.user;
 
-  // Se o usuário tiver o papel de 'admin', conceder acesso total
   if (Boolean(user?.roles?.includes('admin'))) return true;
 
-  // Se o usuário tiver o papel de 'editor', restringir a consulta para incluir apenas o próprio usuário
   if (Boolean(user?.roles?.includes('editor'))) {
     return {
       id: {
@@ -16,10 +24,15 @@ export const isEditorOrOwnsDocument: Access<any, UserType> = ({ req }) => {
     };
   }
 
-  // Para outros casos, negar acesso
   return false;
 };
 
+/**
+ * Verifica se um usuário tem a função de 'editor'.
+ *
+ * @param {Object} req - O objeto de solicitação com informações do usuário.
+ * @returns {boolean} Retorna verdadeiro se o usuário tem o papel de 'editor', caso contrário, retorna falso.
+ */
 export const isEditor: Access<any, UserType> = ({ req }) => {
   return Boolean(req.user?.roles?.includes('editor'));
 };
