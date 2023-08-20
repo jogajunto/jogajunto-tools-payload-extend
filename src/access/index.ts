7e38b8e9de8a11b97ce768bf9ca1235af78e1c75
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-20 02:25:42 -0300

    Create index for access to export all access

---
+++ src/access/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Centralizado de reexportação de verificações de acesso.
+ *
+ * Este arquivo atua como um ponto de entrada único para todas as funções relacionadas
+ * a verificações de acesso, permitindo importações mais limpas em outros lugares
+ * do projeto.
+ */
+export * from './isAdmin';
+export * from './isAuthenticated';
+export * from './isEditor';
