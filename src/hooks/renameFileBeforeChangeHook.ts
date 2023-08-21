c946916522a1ca5aef5160bb5a3533954c7e55ca
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-21 12:17:06 -0300

    Fix hook renameFileBeforeChangeHook for use GlobalBeforeChangeHook

--- src/hooks/renameFileBeforeChangeHook.ts
+++ src/hooks/renameFileBeforeChangeHook.ts
@@ -1,16 +1,11 @@
-import { CollectionBeforeChangeHook } from 'payload/types';
+import { GlobalBeforeChangeHook } from 'payload/types';
 import { format } from '../utilities/formatSlug';
 
-export const renameFileBeforeChangeHook: CollectionBeforeChangeHook = async ({
+export const renameFileBeforeChangeHook: GlobalBeforeChangeHook = async ({
   data,
   req,
-  operation,
   originalDoc,
-  context,
 }) => {
   console.log('data', data);
-  if (operation === 'create' && data.filename) {
-    data.filename = format(data.filename);
-  }
   return data;
 };
