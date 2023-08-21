0ff5de33ced10d09164d32f38d67f812db7bf01b
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-21 15:57:31 -0300

    Create type for FormatterRecord

---
+++ src/types/FormatterRecord.ts
@@ -0,0 +1,7 @@
+import { CollectionName } from './CollectionName';
+import { FormatterCollection } from './FormatterCollection';
+
+export type FormatterRecord<T extends CollectionName> = Record<
+  T,
+  FormatterCollection
+>;
