d5ff86574c0f4e051c245ab5dca2b949305c097a
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-21 12:35:21 -0300

    Fix renameFileBeforeChangeHook hook

--- src/hooks/renameFileBeforeChangeHook.ts
+++ src/hooks/renameFileBeforeChangeHook.ts
@@ -1,12 +1,14 @@
 import { CollectionBeforeChangeHook } from 'payload/types';
 import { format } from '../utilities/formatSlug';
 
-export const renameFileBeforeChangeHook: CollectionBeforeChangeHook = async ({
-  data, // incoming data to update or create with
-  req, // full express request
-  operation, // name of the operation ie. 'create', 'update'
-  originalDoc, // original document
-}) => {
-  console.log('data', data);
-  return data;
+export const renameFileBeforeChangeHook = (): CollectionBeforeChangeHook => {
+  return async ({
+    data, // incoming data to update or create with
+    req, // full express request
+    operation, // name of the operation ie. 'create', 'update'
+    originalDoc, // original document
+  }) => {
+    console.log('data', data);
+    return data;
+  };
 };
