2b9ba7d970937247a5d215c8396ee47adf6d94bc
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-21 15:28:50 -0300

    Generate new build after updates

--- dist/types/FormatterCollection.d.ts
+++ dist/types/FormatterCollection.d.ts
@@ -1,3 +1,3 @@
 import { Document } from 'payload/types';
 import { Payload } from 'payload';
-export type FormatterCollection = (doc: Document, payload: Payload) => Promise<string>;
+export type FormatterCollection = (doc: Document, payload: Payload) => Promise<any>;
