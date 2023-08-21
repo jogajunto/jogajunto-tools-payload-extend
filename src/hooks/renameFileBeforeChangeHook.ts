81a9bc7f0c636cb06be240bb1c524bd51acd1bc9
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-21 10:13:38 -0300

    Fix return async function renameFileBeforeChangeHook

--- src/hooks/renameFileBeforeChangeHook.ts
+++ src/hooks/renameFileBeforeChangeHook.ts
@@ -1,18 +1,14 @@
 import { BeforeChangeHook } from 'payload/dist/collections/config/types';
 import { format } from '../utilities/formatSlug';
 
-export const renameFileBeforeChangeHook: BeforeChangeHook = ({
-  data,
-  req,
-  operation,
-  originalDoc,
-  context,
-}) => {
-  if (operation === 'create') {
-    // Se o arquivo tiver um nome (filename), então formatamos esse nome usando a função formatSlug
-    if (data.filename) {
-      data.filename = format(data.filename);
+export const renameFileBeforeChangeHook: BeforeChangeHook = () => {
+  return async ({ data, req, operation, originalDoc, context }) => {
+    if (operation === 'create') {
+      // Se o arquivo tiver um nome (filename), então formatamos esse nome usando a função formatSlug
+      if (data.filename) {
+        data.filename = format(data.filename);
+      }
     }
-  }
-  return data;
+    return data;
+  };
 };
