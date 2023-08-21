9f8338eef169c90b4cc318a30893274f8b88f5f0
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-21 12:27:10 -0300

    Fix renameFileBeforeChangeHook and re-build

--- dist/hooks/renameFileBeforeChangeHook.d.ts
+++ dist/hooks/renameFileBeforeChangeHook.d.ts
@@ -1,2 +1,2 @@
-import { GlobalBeforeChangeHook } from 'payload/types';
-export declare const renameFileBeforeChangeHook: GlobalBeforeChangeHook;
+import { CollectionBeforeChangeHook } from 'payload/types';
+export declare const renameFileBeforeChangeHook: CollectionBeforeChangeHook;
--- src/hooks/renameFileBeforeChangeHook.ts
+++ src/hooks/renameFileBeforeChangeHook.ts
@@ -1,10 +1,11 @@
-import { GlobalBeforeChangeHook } from 'payload/types';
+import { CollectionBeforeChangeHook } from 'payload/types';
 import { format } from '../utilities/formatSlug';
 
-export const renameFileBeforeChangeHook: GlobalBeforeChangeHook = async ({
-  data,
-  req,
-  originalDoc,
+export const renameFileBeforeChangeHook: CollectionBeforeChangeHook = async ({
+  data, // incoming data to update or create with
+  req, // full express request
+  operation, // name of the operation ie. 'create', 'update'
+  originalDoc, // original document
 }) => {
   console.log('data', data);
   return data;
