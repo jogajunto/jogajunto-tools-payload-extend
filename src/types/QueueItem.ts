43d79e097794a44c8dd7f3eafbbcdc5a93cd7f02
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-24 13:04:58 -0300

    Create type for export QueueItem

---
+++ src/types/QueueItem.ts
@@ -0,0 +1,12 @@
+import { CollectionName } from './CollectionName';
+import { FormatterCollection } from './FormatterCollection';
+import { Document } from 'payload/types';
+
+export interface QueueItem {
+  collection: string;
+  id: string | number;
+  document: Document;
+  operation: 'delete' | 'create' | 'update';
+  directoryRepository: string;
+  collectionFormatters: Record<CollectionName, FormatterCollection>;
+}
