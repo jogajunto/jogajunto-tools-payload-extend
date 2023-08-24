3d365fabbd0b1c1c4720bd00deecb767117182f2
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-24 12:01:22 -0300

    Add manager queue from memory module

---
+++ src/queue/queueManagerMemory.ts
@@ -0,0 +1,123 @@
+// queueManager.ts
+import _ from 'lodash';
+import { Document } from 'payload/types';
+import { CollectionName, FormatterCollection, GitData } from '../types';
+import formatMarkdown from '../utilities/formatMarkdown';
+import payload from 'payload';
+import sendAction from '../utilities/actions/github/sendAction';
+
+interface QueueItem {
+  collection: string;
+  id: string | number;
+  document: Document;
+  operation: 'delete' | 'create' | 'update';
+  directoryRepository: string;
+  collectionFormatters: Record<CollectionName, FormatterCollection>;
+}
+
+const DELAY_BETWEEN_REQUESTS = 9 * 1000; // 9 segundos
+let queue: QueueItem[] = [];
+let isProcessing = false; // Variável de controle
+
+export const itemIsInQueue = (id: string | number): boolean => {
+  return queue.some((item) => item.id === id);
+};
+
+export const addToQueue = (
+  collection: string,
+  id: string | number,
+  document: Document,
+  operation: 'delete' | 'create' | 'update',
+  directoryRepository: string,
+  collectionFormatters: Record<CollectionName, FormatterCollection>
+): void => {
+  const wasEmpty = queue.length === 0;
+  queue.push({
+    collection,
+    id,
+    document,
+    operation,
+    directoryRepository,
+    collectionFormatters,
+  });
+
+  if (wasEmpty && !isProcessing) {
+    processQueue();
+  }
+};
+
+export const removeFromQueue = (
+  collection: string,
+  id: string | number
+): void => {
+  queue = queue.filter(
+    (item) => item.collection !== collection || item.id !== id
+  );
+  console.log('removeFromQueue > queue', queue);
+};
+
+export const getQueue = (): QueueItem[] => {
+  return queue;
+};
+
+export const sendOperationFromGithub = async (task: QueueItem) => {
+  // Formata o documento para markdown
+  const markdownFile = await formatMarkdown(
+    task.document,
+    task.collection,
+    payload,
+    task.collectionFormatters
+  );
+  const data: GitData = {
+    event_type: task.operation,
+    client_payload: {
+      slug: task.document.slug,
+      operation: task.operation,
+      directory: task.directoryRepository,
+      content: _.trim(markdownFile, '\n'),
+    },
+  };
+  sendAction(data);
+};
+
+export const processQueue = async () => {
+  try {
+    if (queue.length === 0) {
+      console.log('Fila está vazia. Saindo do processQueue.');
+      return;
+    }
+
+    const task: QueueItem = queue[0];
+    console.log(
+      'Processando documento da fila:',
+      task.document.id || task.document.slug
+    );
+
+    // Realize a deleção no GitHub aqui
+    await sendOperationFromGithub(task);
+
+    console.log('FAKE - Enviando para o Github', task.document.id);
+
+    // Emular algum processamento ou chamar a função real aqui
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    removeFromQueue(task.collection, task.id);
+
+    if (queue.length > 0) {
+      console.log(
+        'Ainda há itens na fila. Definindo novo tempo para processar o próximo item.'
+      );
+      setTimeout(processQueue, DELAY_BETWEEN_REQUESTS);
+    } else {
+      console.log('Todos os itens processados. Fila limpa.');
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(
+        'Erro durante o processo de execussão da fila:',
+        error.message
+      );
+    } else {
+      console.error('Erro durante o processo de execussão da fila:', error);
+    }
+  }
+};
