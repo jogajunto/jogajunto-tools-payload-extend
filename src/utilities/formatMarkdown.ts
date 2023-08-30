31e9d0de5c661d30681aac865ad19b623a573ffb
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-08-30 13:54:14 -0300

    Valid exists doc.content for create content in markdown

--- src/utilities/formatMarkdown.ts
+++ src/utilities/formatMarkdown.ts
@@ -20,86 +20,88 @@ const formatMarkdown = async (
 
   let content = '\n';
   // Loop in blocks content
-  for (const block of doc.content) {
-    console.log(block.type);
-    switch (block.type) {
-      case 'h2': // Block type H2
-        content += `## ${block.children[0].text}\n\n`;
-        break;
+  if (doc.content) {
+    for (const block of doc.content) {
+      console.log(block.type);
+      switch (block.type) {
+        case 'h2': // Block type H2
+          content += `## ${block.children[0].text}\n\n`;
+          break;
 
-      case 'upload':
-        if (block.value?.id) {
-          // Block type Upload
-          console.log('block_error', block);
-          const idUpload = block.value.id;
-          if (idUpload) {
-            const mediaFile = await payload.findByID({
-              collection: 'media',
-              id: idUpload,
-            });
-            if (mediaFile) {
-              const filename = mediaFile.filename;
-              const urlFile = mediaFile.url;
-
-              content += `![${filename}](${urlFile})\n`;
-            }
-          }
-        }
-        break;
-
-      case 'ul':
-        // Block type UL
-        block.children.forEach((listItem: any) => {
-          content += '- ';
-          listItem.children.forEach((child: any) => {
-            let text = child.text;
-
-            if (child.bold) {
-              text = `**${text}**`;
-            }
-
-            if (child.italic) {
-              text = `_${text}_`;
-            }
-
-            if (child.type === 'link') {
-              child.children.forEach((linkChild: any) => {
-                text = `[${linkChild.text}](${child.url})`;
+        case 'upload':
+          if (block.value?.id) {
+            // Block type Upload
+            console.log('block_error', block);
+            const idUpload = block.value.id;
+            if (idUpload) {
+              const mediaFile = await payload.findByID({
+                collection: 'media',
+                id: idUpload,
               });
+              if (mediaFile) {
+                const filename = mediaFile.filename;
+                const urlFile = mediaFile.url;
+
+                content += `![${filename}](${urlFile})\n`;
+              }
             }
-
-            content += _.trim(text, '\n');
-          });
-          content += '\n';
-        });
-        break;
-
-      default:
-        if (block.children.text != '') {
-          // Block type any
-          block.children.forEach((child: any) => {
-            let text = child.text;
-
-            if (child.bold) {
-              text = `**${text}**`;
-            }
-
-            if (child.type === 'link') {
-              text = `[${text}](${child.url})`;
-            }
-
-            if (child.italic) {
-              text = `_${text}_`;
-            }
-
-            content += _.trim(text, '\n');
-          });
-
-          if (block.children.length > 0) {
-            content += '\n\n'; // Add two newlines to create a new paragraph in Markdown
           }
-        }
-        break;
+          break;
+
+        case 'ul':
+          // Block type UL
+          block.children.forEach((listItem: any) => {
+            content += '- ';
+            listItem.children.forEach((child: any) => {
+              let text = child.text;
+
+              if (child.bold) {
+                text = `**${text}**`;
+              }
+
+              if (child.italic) {
+                text = `_${text}_`;
+              }
+
+              if (child.type === 'link') {
+                child.children.forEach((linkChild: any) => {
+                  text = `[${linkChild.text}](${child.url})`;
+                });
+              }
+
+              content += _.trim(text, '\n');
+            });
+            content += '\n';
+          });
+          break;
+
+        default:
+          if (block.children.text != '') {
+            // Block type any
+            block.children.forEach((child: any) => {
+              let text = child.text;
+
+              if (child.bold) {
+                text = `**${text}**`;
+              }
+
+              if (child.type === 'link') {
+                text = `[${text}](${child.url})`;
+              }
+
+              if (child.italic) {
+                text = `_${text}_`;
+              }
+
+              content += _.trim(text, '\n');
+            });
+
+            if (block.children.length > 0) {
+              content += '\n\n'; // Add two newlines to create a new paragraph in Markdown
+            }
+          }
+          break;
+      }
     }
   }
 
