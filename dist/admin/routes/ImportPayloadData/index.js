2d7521feeb93070cbec9800f00aa284ddc73325e
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-05 23:09:55 -0300

    Generate new build after updates

--- dist/admin/routes/ImportPayloadData/index.js
+++ dist/admin/routes/ImportPayloadData/index.js
@@ -74,11 +74,13 @@ const ImportPayloadData = (req, res) => {
                         data: item,
                     });
                 }
-                res.status(200).send({ message: 'Dados importados com sucesso.' });
+                return res
+                    .status(200)
+                    .send({ message: 'Dados importados com sucesso.' });
             }
             catch (error) {
                 console.error('Erro ao processar e importar dados para o Payload CMS:', error);
-                res.status(500).send({ message: 'Erro interno do servidor.' });
+                return res.status(500).send({ message: 'Erro interno do servidor.' });
             }
         });
     }
