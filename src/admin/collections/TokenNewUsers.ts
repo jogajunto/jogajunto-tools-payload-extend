7a461935262f49f0328b593e01a14ea82635b4b1
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-10 20:27:42 -0300

    Add fixed collection for TokenNewUsers

---
+++ src/admin/collections/TokenNewUsers.ts
@@ -0,0 +1,46 @@
+import { isAdmin, isAdminFieldLevel } from '../../access/isAdmin';
+import { CollectionConfig } from 'payload/types';
+import isNewUserForToken from '../../access/isNewUserForToken';
+import TokenGenerator from '../components/TokenGenerator';
+
+const TokenNewUsers: CollectionConfig = {
+  slug: 'tokennewusers',
+  labels: {
+    singular: 'Token de Novo Usuário',
+    plural: 'Tokens para Novos Usuários',
+  },
+  access: {
+    create: isAdmin,
+    read: isAdmin ?? isNewUserForToken,
+  },
+  admin: {
+    useAsTitle: 'id',
+    disableDuplicate: true,
+    hideAPIURL: true,
+  },
+  fields: [
+    {
+      name: 'token',
+      label: 'Token JWT',
+      type: 'text',
+      required: true,
+      unique: true,
+      access: {
+        create: isAdminFieldLevel,
+        update: isAdminFieldLevel,
+      },
+      admin: {
+        description: 'Gere um token para um link de cadastro',
+        components: {
+          Field: TokenGenerator,
+        },
+      },
+    },
+    {
+      name: 'used',
+      type: 'checkbox',
+    },
+  ],
+};
+
+export default TokenNewUsers;
