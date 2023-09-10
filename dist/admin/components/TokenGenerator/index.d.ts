4eca40e251e34a4df7d0c4fa9d6fcdddf233ccfb
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-10 20:36:03 -0300

    Generate new build after updates

--- dist/admin/components/TokenGenerator/index.d.ts
+++ dist/admin/components/TokenGenerator/index.d.ts
@@ -1,4 +1,3 @@
 import React from 'react';
-import './index.scss';
 declare const TokenGenerator: React.FC<any>;
 export default TokenGenerator;
--- dist/admin/components/TokenGenerator/index.js
+++ dist/admin/components/TokenGenerator/index.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const forms_1 = require("payload/components/forms");
-require("./index.scss");
 const utilities_1 = require("payload/components/utilities");
 const baseClass = 'tokenGenerator';
 const TokenGenerator = ({ path, label, required }) => {
