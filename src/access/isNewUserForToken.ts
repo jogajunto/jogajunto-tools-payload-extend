b27c0146f132967dc2de586fada723c31cbe8421
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-10 23:24:49 -0300

    Fix!: isNewUserForToken is valid case exists req.body?.token or user admin role

--- src/access/isNewUserForToken.ts
+++ src/access/isNewUserForToken.ts
@@ -2,22 +2,24 @@ import { Access } from 'payload/config';
 
 const isNewUserForToken: Access<any, any> = async ({ req, id, data }) => {
   /**
-   * Case user editor return false
-   */
-  if (req.user)
-    if (req.user?.roles.includes('editor') || req.user?.roles.includes('admin'))
-      return Boolean(false);
-  /**
    * In create new user for valid token return true
    */
-  const response = await req.payload.find({
-    collection: 'tokennewusers',
-    where: {
-      token: { equals: req.body.token },
-      used: { equals: false || undefined },
-    },
-  });
-  if (response.docs.length > 0) return Boolean(true);
+  if (req.body?.token) {
+    if (req.user) return Boolean(false);
+    const response = await req.payload.find({
+      collection: 'tokennewusers',
+      where: {
+        token: { equals: req.body.token },
+        used: { equals: false || undefined },
+      },
+    });
+    if (response.docs.length > 0) return Boolean(true);
+  } else {
+    /**
+     * Case user editor return false
+     */
+    if (req.user) if (req.user?.roles.includes('admin')) return Boolean(true);
+  }
   /**
    * Another return false
    */
