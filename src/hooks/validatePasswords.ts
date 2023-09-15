ae5c645d6f5d60f9a968bbca853c6961b09bec03
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-15 12:30:31 -0300

    Fix condition validation password and format document automatic

--- src/hooks/validatePasswords.ts
+++ src/hooks/validatePasswords.ts
@@ -1,14 +1,14 @@
 /**
  * @module hooks/validatePasswords
- * @description Este módulo fornece um hook que valida senhas conforme critérios específicos 
- * antes da criação ou atualização de um documento no Payload CMS. O objetivo é garantir 
+ * @description Este módulo fornece um hook que valida senhas conforme critérios específicos
+ * antes da criação ou atualização de um documento no Payload CMS. O objetivo é garantir
  * que as senhas definidas pelos usuários atendam a um conjunto de regras de segurança.
- * 
+ *
  * Esse módulo ajuda a garantir que os usuários do sistema definam senhas robustas, aumentando
  * a segurança do sistema.
  */
 
-import { CollectionBeforeChangeHook } from "payload/types";
+import { CollectionBeforeChangeHook } from 'payload/types';
 
 /**
  * Este hook valida as senhas de acordo com os seguintes critérios:
@@ -20,43 +20,43 @@ import { CollectionBeforeChangeHook } from "payload/types";
  * - A senha deve conter pelo menos um caractere especial (como !, @, #, etc.).
  *
  * Se a senha não atender a qualquer um desses critérios, um erro é lançado.
- * 
+ *
  * @returns Os dados originais se a senha for válida.
  * @throws {Error} Se a senha for inválida de acordo com os critérios estabelecidos.
  */
 
 export const validatePasswords: CollectionBeforeChangeHook = async ({
-    data,
-    req: { payload },
-    operation,
-    originalDoc,
-  }) => {
-    if (operation === 'create' || (operation === 'update' && data.password)) {
-      const MIN_LENGTH = 8;
-  
-      if (typeof data.password !== 'string') {
-        throw new Error('A senha deve ser uma string.');
-      }
-  
-      if (data.password.length < MIN_LENGTH) {
-        throw new Error('A senha deve ter pelo menos 8 caracteres.');
-      }
+  data,
+  req: { payload },
+  operation,
+  originalDoc,
+}) => {
+  if (operation === 'create' || (operation === 'update' && data.password)) {
+    const MIN_LENGTH = 8;
 
-      if (!/\d/.test(data.password)) {
-        throw new Error('A senha deve conter pelo menos um número.');
-      }
-
-      if (!/[a-z]/.test(data.password)) {
-        throw new Error('A senha deve conter pelo menos uma letra minúscula.');
-      }
-
-      if (!/[A-Z]/.test(data.password)) {
-        throw new Error('A senha deve conter pelo menos uma letra maiúscula.');
-      }
- 
-      if (!/[!@#$%^&*(),.?":{}|<>]/.test(data.password)) {
-        throw new Error('A senha deve conter pelo menos um caractere especial.');
-      }
+    if (typeof data.password !== 'string') {
+      throw new Error('A senha deve ser uma string.');
     }
-    return data;
-  };
+
+    if (data.password.length < MIN_LENGTH) {
+      throw new Error('A senha deve ter pelo menos 8 caracteres.');
+    }
+
+    if (!/\d/.test(data.password)) {
+      throw new Error('A senha deve conter pelo menos um número.');
+    }
+
+    if (!/[a-z]/.test(data.password)) {
+      throw new Error('A senha deve conter pelo menos uma letra minúscula.');
+    }
+
+    if (!/[A-Z]/.test(data.password)) {
+      throw new Error('A senha deve conter pelo menos uma letra maiúscula.');
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>_-]/.test(data.password)) {
+      throw new Error('A senha deve conter pelo menos um caractere especial.');
+    }
+  }
+  return data;
+};
