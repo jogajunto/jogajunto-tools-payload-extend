9d825cdc92ccb53fe523d6a67e95b97264e28efe
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-19 01:16:56 -0300

    Fix redundant condition in access/isNewUserForToken

--- src/access/isNewUserForToken.ts
+++ src/access/isNewUserForToken.ts
@@ -2,14 +2,14 @@ import { Access } from 'payload/config';
 
 /**
  * Verifica se um novo usuário pode ser criado para um token fornecido.
- * 
+ *
  * @function
  * @async
  * @param {Object} args - Argumentos passados para a função.
  * @param {Object} args.req - Objeto de requisição express.
  * @param {string} args.id - ID associado ao usuário.
  * @param {Object} args.data - Dados relacionados à requisição.
- * 
+ *
  * @returns {boolean} Retorna `true` se um novo usuário pode ser criado para o token fornecido. Caso contrário, retorna `false`.
  */
 const isNewUserForToken: Access<any, any> = async ({ req, id, data }) => {
@@ -24,9 +24,9 @@ const isNewUserForToken: Access<any, any> = async ({ req, id, data }) => {
       },
     });
     if (response.docs.length > 0) return Boolean(true);
-  } else {
-    // No caso de um usuário editor, retorna falso
-    if (req.user) if (req.user?.roles.includes('admin')) return Boolean(true);
+  } // Se o usuário existe e é um admin
+  else if (req.user && req.user?.roles.includes('admin')) {
+    return Boolean(true);
   }
   // Para qualquer outro caso, retorna falso
   return Boolean(false);
