f4c9394bdd49f6007f80154066fb19bdcb3ed01c
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-18 23:00:17 -0300

    Create tests from access/isNewUserForToken

---
+++ tests/access/isNewUserForToken.test.ts
@@ -0,0 +1,73 @@
+import isNewUserForToken from '../../dist/access/isNewUserForToken'; // Atualize para o caminho correto
+
+describe('isNewUserForToken', () => {
+  // Ao criar um novo usuário para um token válido
+  it('should return true for valid token', async () => {
+    const mockReq = {
+      body: { token: 'validToken' },
+      user: undefined,
+      payload: {
+        find: jest.fn().mockResolvedValue({ docs: [{}] }),
+      },
+    };
+
+    const result = await isNewUserForToken({ req: mockReq });
+    expect(result).toBe(true);
+  });
+
+  // Quando um usuário existente tenta usar um token válido
+  it('should return false for an existing user trying with valid token', async () => {
+    const mockReq = {
+      body: { token: 'validToken' },
+      user: { id: 'someUserId' },
+      payload: {
+        find: jest.fn().mockResolvedValue({ docs: [{}] }),
+      },
+    };
+
+    const result = await isNewUserForToken({ req: mockReq });
+    expect(result).toBe(false);
+  });
+
+  // Quando um token inválido é usado
+  it('should return false for an invalid token', async () => {
+    const mockReq = {
+      body: { token: 'invalidToken' },
+      user: undefined,
+      payload: {
+        find: jest.fn().mockResolvedValue({ docs: [] }),
+      },
+    };
+
+    const result = await isNewUserForToken({ req: mockReq });
+    expect(result).toBe(false);
+  });
+
+  // Quando um usuário com papel de admin está criando
+  it('should return true for an admin user', async () => {
+    const mockReq = {
+      body: {},
+      user: { roles: ['admin'] },
+      payload: {
+        find: jest.fn(),
+      },
+    };
+
+    const result = await isNewUserForToken({ req: mockReq });
+    expect(result).toBe(true);
+  });
+
+  // Para qualquer outro caso
+  it('should return false for any other case', async () => {
+    const mockReq = {
+      body: {},
+      user: { roles: ['editor'] },
+      payload: {
+        find: jest.fn(),
+      },
+    };
+
+    const result = await isNewUserForToken({ req: mockReq });
+    expect(result).toBe(false);
+  });
+});
