cabfab0019e17b037b070d6e867983fc05740214
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-09-18 22:17:13 -0300

    Create tests from access/isAdmin

---
+++ tests/access/isAdmin.test.ts
@@ -0,0 +1,57 @@
+import { isAdmin, isAdminFieldLevel } from '../../src/access/isAdmin';
+
+describe('isAdmin', () => {
+  it('should return true if user has admin role', () => {
+    const mockReq = {
+      user: {
+        name: 'Teste',
+        email: 'test@test.com',
+        roles: ['admin'],
+      },
+    };
+
+    const result = isAdmin({ req: mockReq });
+    expect(result).toBe(true);
+  });
+
+  it('should return false if user does not have admin role', () => {
+    const mockReq = {
+      user: {
+        name: 'Teste',
+        email: 'test@test.com',
+        roles: ['editor'],
+      },
+    };
+
+    const result = isAdmin({ req: mockReq });
+    expect(result).toBe(false);
+  });
+});
+
+describe('isAdminFieldLevel', () => {
+  it('should return true if user has admin role', () => {
+    const mockReq = {
+      user: {
+        name: 'Teste',
+        email: 'test@test.com',
+        roles: ['admin'],
+      },
+    };
+
+    const result = isAdminFieldLevel({ req: mockReq });
+    expect(result).toBe(true);
+  });
+
+  it('should return false if user does not have admin role', () => {
+    const mockReq = {
+      user: {
+        name: 'Teste',
+        email: 'test@test.com',
+        roles: ['editor'],
+      },
+    };
+
+    const result = isAdminFieldLevel({ req: mockReq });
+    expect(result).toBe(false);
+  });
+});
