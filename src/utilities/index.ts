c03743d3221c0276308fe2a52fb09bf470ccab8d
rafaell1995 <rafaeldearaujop95@gmail.com>
2024-03-03 22:39:15 -0300

    Fix exports in utilities index

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -3,31 +3,43 @@
  * @description Funções utilitárias e ajudantes para o projeto 'jogajunto-tools-payload-extend'.
  */
 
-/** 
+/**
  * Exportando a função utilitária para formatar conteúdo como Markdown.
  * @see module:formatMarkdown
  */
 export * from './formatMarkdown';
 
-/** 
+/**
+ * Exportando a função utilitária para formatar conteúdo como Markdown V2.
+ * @see module:formatMarkdownV2
+ */
+export * from './formatMarkdownV2';
+
+/**
+ * Exportando a função utilitária para resgatar os arquivos da coleção de upload na hora da exportação de conteúdo.
+ * @see module:imagesForConvertNodeToMarkdown
+ */
+export * from './imagesForConvertNodeToMarkdown';
+
+/**
  * Exportando funções utilitárias relacionadas à formatação de slugs.
  * @see module:formatSlug
  */
 export * from './formatSlug';
 
-/** 
+/**
  * Exportando a função utilitária para preparar imagens para o repositório.
  * @see module:prepareImageForRepository
  */
 export * from './prepareImageForRepository';
 
-/** 
+/**
  * Exportando a função utilitária para formatar uma string como slug.
  * @see module:formatSlugString
  */
 export * from './formatSlugString';
 
-/** 
+/**
  * Exportando a função utilitária para dividir nomes de arquivos.
  * @see module:splitFilename
  */
